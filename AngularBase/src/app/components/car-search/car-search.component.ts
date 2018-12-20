1091ab812385282c0b1b2859e3dc3e64deb43f09
Kyle Smith <coburnroar@gmail.com>
2018-12-19 18:24:10 -0600

    Fixed Syntax Errors

--- AngularBase/src/app/components/car-search/car-search.component.ts
+++ AngularBase/src/app/components/car-search/car-search.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { CarSearch } from './car-search.ts'
+import { CarSearch } from './car-search'
 
 @Component({
   selector: 'car-search',
   templateUrl: './car-search.component.html',
   styleUrls: ['./car-search.component.css']
 })
-export class CarSearchComponent implements OnInit {
+export class CarSearchComponent {
   
   colors=[null,'Red','White','Grey','Silver','Black']
   selector=[null,'Yes','No']
